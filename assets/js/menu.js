3cffd45b8288d5e2fb5220d6f6f22f2441aa77ba
ShotMeow <shotmeow@gmail.com>
2022-04-21 15:57:01 +0300

    Added Animation

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,6 +2,7 @@ export const menuVisibility = () => {
     const button = document.querySelector('.header__button')
     const list = document.querySelector('.nav__list')
     button.addEventListener('click', () => {
-        list.classList.toggle('hidden')
+        list.classList.toggle('hidden-full')
+        button.classList.toggle('active')
     })
 }
